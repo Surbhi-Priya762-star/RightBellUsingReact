42479f9c9bfb399e4c16f2db69ffc340b669dba0
rightBell <rightbell1@gmail.com>
2021-08-19 22:46:09 +0530

    commit6

---
+++ src/My Components/SignUp/ServiceUser.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Header from '../Header/Header';
+
+function ServiceUser() {
+  return (
+    <div>
+      <Header/>
+     SignupServiceUserForm
+    </div>
+  )
+}
+
+export default ServiceUser;
